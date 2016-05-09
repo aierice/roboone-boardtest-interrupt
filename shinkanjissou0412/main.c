724cacf5808b61645777d8b830df03a747230611
Aierice <pokopenmtok@yahoo.co.jp>
2016-05-09 23:42:28 +0900

    不要なコメントの削除
    githubで管理する上で不要なコメントを削除．

--- shinkanjissou0412/main.c
+++ shinkanjissou0412/main.c
@@ -1,5 +1,3 @@
-//4/29 関数を整理．usart1の送信，モータの動作を確認．usart3の送信が確認できず．
-
 #include"stm32f4xx.h"
 #include"initialsetting.h"
 #include"make_motion.h"
@@ -13,8 +11,6 @@ int main(void)
 	NVIC_Configuration();
 	USART1_Configuration();
 	USART3_Configuration();
-//	USART_Cmd( USART1, ENABLE);		//0502なぜか残っていたけど，要らない
-//	USART_Cmd( USART3, ENABLE);
 
 	if(SysTick_Config(SystemCoreClock / 1000)){
 		while(1);
@@ -25,10 +21,7 @@ int main(void)
     {
 		while(!GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0));
 		send_exp_USART1();
-//		do_motion(0b1000000100000001);
-//		GPIO_SetBits(GPIOA,GPIO_Pin_11);
     	tdelay(500);
-//   	GPIO_ResetBits(GPIOA,GPIO_Pin_11);
     }
 }
 
--- shinkanjissou0412/usart_handler.c
+++ shinkanjissou0412/usart_handler.c
@@ -7,8 +7,7 @@ void USART3_IRQHandler( void){
 	uint16_t command = 0;
 	static uint16_t commandfull = 0;
 	if( USART_GetITStatus( USART3,USART_IT_RXNE) != RESET){
-//		USART_ClearFlag( USART3,USART_FLAG_RXNE);			//0502これいらなさそう
-		USART_ClearITPendingBit( USART3, USART_IT_RXNE);	//0502追加．でもこれ書いてないサンプルもあるし……
+		USART_ClearITPendingBit( USART3, USART_IT_RXNE);
 /*		command = ( uint8_t)USART_ReceiveData( USART3);
 		if(command & 0b0000000010000000 || !commandfull){
 			commandfull = command<<8;
