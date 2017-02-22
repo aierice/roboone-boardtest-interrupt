2b4fb1b8eb80e5529b6d98db819a4efd67486d57
Aierice <pokopenmtok@yahoo.co.jp>
2017-02-22 22:10:09 +0900

    boardrateを57600へ

--- shinkanjissou0412/initialsetting.c
+++ shinkanjissou0412/initialsetting.c
@@ -63,7 +63,7 @@ void USART1_Configuration( void){
 	GPIO_PinAFConfig(GPIOB , GPIO_PinSource7 , GPIO_AF_USART1);
 
 	USART_InitTypeDef USART_InitStructure;
-	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_BaudRate = 57600;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
